10802600c69617f619280d14dd9814a1879f1dac
Ilya Bondarchuk <cod3provider@gmail.com>
2024-10-21 20:42:27 +0200

    added RegisterScreen

---
+++ screens/RegistrationScreen/RegistrationScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Image, ScrollView, ImageBackground} from "react-native";
+import {REGISTER_SCREEN_STYLES} from './styles';
+import RegisterForm from "@/components/RegisterForm/RegisterForm";
+
+export default function RegistrationScreen() {
+  return (
+    <View style={REGISTER_SCREEN_STYLES.container}>
+      <ImageBackground
+        source={require('@/assets/images/Photo_BG.png')}
+        resizeMode="cover"
+        style={REGISTER_SCREEN_STYLES.backgroundImage}
+      />
+        <RegisterForm/>
+    </View>
+  )
+}
