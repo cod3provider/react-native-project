82ba0d37c6f8029f9263ed6a732d8b01f769a0e8
Ilya Bondarchuk <cod3provider@gmail.com>
2024-11-08 18:27:47 +0100

    added buttons with icons

---
+++ components/LogoutButton.tsx
@@ -0,0 +1,17 @@
+import {ButtonProps, TouchableOpacity} from "react-native";
+import {FC} from "react";
+import LogoutIcon from "@/assets/icons/LogoutIcon";
+
+type Props = {
+  onPress: () => void;
+}
+
+const LogoutButton: FC<Props> = ({onPress}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <LogoutIcon />
+    </TouchableOpacity>
+  )
+}
+
+export default LogoutButton;
