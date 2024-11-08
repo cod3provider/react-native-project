3cfaf5dbbc516419d3504a81e79644a8a2dc2d11
Ilya Bondarchuk <cod3provider@gmail.com>
2024-11-08 18:28:39 +0100

    added StackNavigator

---
+++ assets/icons/LogoutIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react"
+import Svg, { SvgProps, Path } from "react-native-svg"
+const SvgComponent = (props: SvgProps) => (
+  <Svg
+    width={24}
+    height={24}
+    fill="none"
+    {...props}
+  >
+    <Path
+      stroke="#BDBDBD"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M10 22H5a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h5M17 16l4-4-4-4M21 12H9"
+    />
+  </Svg>
+)
+export default SvgComponent
