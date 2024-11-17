b1726c64a521b4267837aae6d8f3d46140f2fcad
Ilya Bondarchuk <cod3provider@gmail.com>
2024-11-17 20:33:01 +0100

    added logic for rendering posts

--- screens/PostsScreen/PostsScreen.tsx
+++ screens/PostsScreen/PostsScreen.tsx
@@ -1,7 +1,26 @@
+import React from "react";
 import {Image, Text, View} from "react-native";
 import styles from "@/screens/PostsScreen/styles";
 
-const PostsScreen = () => {
+type Post = {
+  name: string;
+  location: string;
+  photoPicture: string;
+};
+
+// type PostsScreenRouteProp = RouteProp<{ Posts: {posts: Post[] }}, 'Posts'>;
+//
+// type PostsScreenProps = {
+//   route: PostsScreenRouteProp
+// }
+
+type PostsScreenProps = {
+  posts: Post[];
+};
+
+const PostsScreen: React.FC<PostsScreenProps> = ({ posts }) => {
+  // const {posts} = route.params;
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -11,6 +30,23 @@ const PostsScreen = () => {
           <Text style={styles.subtitle}>email@example.com</Text>
         </View>
       </View>
+        {posts?.length > 0 ? (
+          posts.map((post, index) => (
+            <View style={styles.post} key={index}>
+              <Image source={{uri: post.photoPicture}} style={styles.postImage} />
+              <Text>{post.name}</Text>
+              <View style={styles.text}>
+                <View>
+
+                  0
+                </View>
+                <Text>{post.location}</Text>
+              </View>
+            </View>
+          ))
+        ) : (
+          <Text> No posts</Text>
+        )}
     </View>
   )
 }
